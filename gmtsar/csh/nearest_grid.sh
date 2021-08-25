841cf331ef253b94d2b04272ff59e3afca8c27a7
Alexey Pechnikov <pechnikov@mobigroup.ru>
2021-08-25 12:33:07 +0700

    Fast replacement for original binary tool nearest_grid

---
+++ gmtsar/csh/nearest_grid.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Replacement for /usr/local/GMTSAR/bin/nearest_grid
+set -e
+
+if [ "$#" -ne "2" -a "$#" -ne "3" ]
+then
+    echo "Usage: nearest_grid input.grd output.grd [search_radius_pixels]"
+    exit
+fi
+
+input="$1"
+output="$2"
+radius="$3"
+
+# set default search radius, pixels
+if [ -z "$radius" ]
+then
+    radius=300
+fi
+
+gdal_fillnodata.py -md "$radius" "${input}" "${output}.tif"
+gmt grdreformat "${output}.tif" "${output}"
+bounds=$(gmt grdinfo -C "${input}" | cut -s -f2,3,4,5 | awk 'BEGIN{OFS = "/"}{print ($1,$2,$3,$4)}')
+gmt grdedit "${output}" -R"$bounds" -T
+rm -f "${output}.tif"
