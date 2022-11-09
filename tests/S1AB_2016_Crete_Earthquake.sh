05143b41040a41d9f0180b75bd1018ea6bf938f0
Alexey Pechnikov <pechnikov@mobigroup.ru>
2022-11-09 16:35:31 +0700

    Update test case for the recent code changes and add wrapper shell script to download the dataset and start the test

---
+++ tests/S1AB_2016_Crete_Earthquake.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e
+
+if [ "$#" -eq 0 ]
+then
+    echo "Default launch saves downloaded dataset for future usage"
+    force=0
+elif [ "$#" -eq 1 ]
+then
+    echo "Force launch removes downloaded dataset to save disk space"
+    force=1
+else
+    echo "Incorrect number of arguments"
+    exit 1
+fi
+
+echo "Downloading iCloud datasets..."
+cat > icloud_urls.txt <<- END
+https://www.icloud.com/iclouddrive/060EcVsMZF5o5nDoyAxHZ-JgQ#S1A_IW_SLC__1SDV_20210929T162245_20210929T162312_039899_04B89A_651D.zip
+https://www.icloud.com/iclouddrive/0bcQQkK2QDSiX76_y1O6zfd_Q#S1B_IW_SLC__1SDV_20210923T162204_20210923T162231_028828_0370C4_681D.zip
+https://www.icloud.com/iclouddrive/085_X__5ZszrqcnP--SGCH-tw#S1AB_Crete_Earthquake_vs_geObsevatory.jpg
+END
+cut -f5 -d '/' icloud_urls.txt | cut -f1 -d '#' | sed -E 's/(.*)/{"shortGUIDs":[{"value":"\1"}]}/' > requests.txt
+cut -f5 -d '/' icloud_urls.txt | cut -f2 -d '#' > fnames.txt
+paste -d ' ' requests.txt fnames.txt  | while read LINE
+do
+    request=$(echo "$LINE"| cut -d ' ' -f1)
+    fname=$(echo "$LINE"| cut -d ' ' -f2)
+    echo "request: $request fname: $fname"
+    url=$(curl -s 'https://ckdatabasews.icloud.com/database/1/com.apple.cloudkit/production/public/records/resolve' \
+    --data-raw "${request}" --compressed | jq -r '.results[0].rootRecord.fields.fileContent.value.downloadURL')
+    # continue downloading or miss downloading when the complete file already exists
+    wget -c -O "${fname}" "${url}"
+done
+echo "Unpacking selected files only to data directory..."
+mkdir -p data
+unzip -j -n S1A_IW_SLC__1SDV_20210929T162245_20210929T162312_039899_04B89A_651D.zip \
+    '*.SAFE/*/s1?-iw3-slc-vv-*-006.xml' -d data
+unzip -j -n S1A_IW_SLC__1SDV_20210929T162245_20210929T162312_039899_04B89A_651D.zip \
+    '*.SAFE/*/s1?-iw3-slc-vv-*-006.tiff' -d data
+unzip -j -n S1B_IW_SLC__1SDV_20210923T162204_20210923T162231_028828_0370C4_681D.zip \
+    '*.SAFE/*/s1?-iw3-slc-vv-*-006.xml' -d data
+unzip -j -n S1B_IW_SLC__1SDV_20210923T162204_20210923T162231_028828_0370C4_681D.zip \
+    '*.SAFE/*/s1?-iw3-slc-vv-*-006.tiff' -d data
+# cleanup
+if [ "$force" -eq 1 ]
+then
+    echo "Removing downloaded datasets to free disk space following 'force' command line argument'..."
+    rm *.zip
+fi
+echo "Checking disk space and downloaded files"
+df -h
+ls -lh
+ls -lh data
+echo "Running Python test script..."
+python3 ./S1AB_2021_Crete_Earthquake.py && echo SUCCESS
--- tests/S1AB_2021_Crete_Earthquake.py
+++ tests/S1AB_2021_Crete_Earthquake.py
@@ -5,6 +5,8 @@
 from pygmtsar import SBAS
 import numpy as np
 import xarray as xr
+from dask.distributed import Client
+import matplotlib.pyplot as plt
 
 #Define Parameters
 WORKDIR      = 'raw'
@@ -12,51 +14,57 @@ DATADIR      = 'data'
 CORRLIMIT    = 0.10
 DEFOMAX      = 0
 
-# Init SBAS
-sbas = SBAS(DATADIR, basedir=WORKDIR)
-print (sbas.to_dataframe())
-# Download orbits
-sbas.download_orbits()
-print (sbas.to_dataframe())
-# Reframe scenes
-sbas.set_pins([25.3, 35.0, None, None])
-sbas.reframe_parallel()
-print (sbas.get_pins())
-sbas.to_dataframe()
-# Download DEM
-#sbas.download_dem(backend='GMT', product='SRTM3')
-sbas.download_dem(product='SRTM1')
-print (sbas.get_dem())
-# Align a Stack of Images
-sbas.stack_parallel()
-# DEM in Radar Coordinates
-sbas.topo_ra_parallel()
-print (sbas.get_topo_ra())
-#Interferograms
-pairs = [sbas.to_dataframe().index]
-#decimator = lambda dataarray: dataarray.coarsen({'y': 4, 'x': 4}, boundary='trim').mean()
-# use the DEM default resolution 60 m
-decimator = sbas.pixel_decimator(resolution_meters=60, debug=True)
-sbas.intf_parallel(pairs, func=decimator)
-print (pairs)
-print (sbas.open_grids(pairs, 'phasefilt'))
-print (sbas.open_grids(pairs, 'phasefilt', geocode=True))
-print (sbas.open_grids(pairs, 'corr'))
-# Download landmask
-sbas.download_landmask()
-print (sbas.get_landmask())
-landmask_ll = sbas.get_landmask()
-print (sbas.open_grids(pairs, 'phasefilt', mask=landmask_ll, geocode=True)[0])
-# Unwrapping
-landmask_ra = sbas.get_landmask(inverse_geocode=True)
-cleaner = lambda corr, unwrap: xr.where(corr>=CORRLIMIT, unwrap, np.nan)
-sbas.unwrap_parallel(pairs, threshold=CORRLIMIT, mask=landmask_ra, func=cleaner)
-print (sbas.open_grids(pairs, 'unwrap', geocode=True)[0])
-print (sbas.open_grids(pairs, 'unwrap', func=sbas.los_displacement_mm, geocode=True)[0])
-# Incidence angle
-sbas.sat_look_parallel()
-print (sbas.incidence_angle())
-# Displacements
-print (sbas.open_grids(pairs, 'unwrap', func=sbas.los_displacement_mm, geocode=True)[0])
-print (sbas.open_grids(pairs, 'unwrap', func=[sbas.vertical_displacement_mm, sbas.nearest_grid], mask=sbas.intf_ra2ll(landmask_ra), geocode=True)[0])
-print (sbas.open_grids(pairs, 'unwrap', func=[sbas.eastwest_displacement_mm, sbas.nearest_grid], mask=sbas.intf_ra2ll(landmask_ra), geocode=True)[0])
+if __name__ == '__main__':
+    # Run Local Dask Cluster
+    client = Client()
+    print (client)
+    # Init SBAS
+    sbas = SBAS(DATADIR, basedir=WORKDIR)
+    print (sbas.to_dataframe())
+    # Download orbits
+    sbas.download_orbits()
+    print (sbas.to_dataframe())
+    # Reframe scenes
+    sbas.set_pins([25.3, 35.0, None, None])
+    sbas.reframe_parallel()
+    print (sbas.get_pins())
+    sbas.to_dataframe()
+    # Download DEM
+    #sbas.download_dem(backend='GMT', product='SRTM3')
+    sbas.download_dem(product='SRTM1')
+    print (sbas.get_dem())
+    # Align a Stack of Images
+    sbas.stack_parallel()
+    # DEM in Radar Coordinates
+    sbas.topo_ra_parallel()
+    print (sbas.get_topo_ra())
+    #Interferograms
+    pairs = [sbas.to_dataframe().index]
+    #decimator = lambda dataarray: dataarray.coarsen({'y': 4, 'x': 4}, boundary='trim').mean()
+    # use the DEM default resolution 60 m
+    decimator = sbas.pixel_decimator(resolution_meters=60, debug=True)
+    sbas.intf_parallel(pairs, func=decimator)
+    # geocode matrices
+    sbas.geocode_parallel(pairs)
+    print (pairs)
+    print (sbas.open_grids(pairs, 'phasefilt'))
+    print (sbas.open_grids(pairs, 'phasefilt', geocode=True))
+    print (sbas.open_grids(pairs, 'corr'))
+    # Download landmask
+    sbas.download_landmask()
+    print (sbas.get_landmask())
+    landmask_ll = sbas.get_landmask()
+    print (sbas.open_grids(pairs, 'phasefilt', mask=landmask_ll, geocode=True)[0])
+    # Unwrapping
+    landmask_ra = sbas.get_landmask(inverse_geocode=True)
+    cleaner = lambda corr, unwrap: xr.where(corr>=CORRLIMIT, unwrap, np.nan)
+    sbas.unwrap_parallel(pairs, threshold=CORRLIMIT, mask=landmask_ra, func=cleaner)
+    print (sbas.open_grids(pairs, 'unwrap', geocode=True)[0])
+    print (sbas.open_grids(pairs, 'unwrap', func=sbas.los_displacement_mm, geocode=True)[0])
+    # Incidence angle
+    sbas.sat_look_parallel()
+    print (sbas.incidence_angle())
+    # Displacements
+    print (sbas.open_grids(pairs, 'unwrap', func=sbas.los_displacement_mm, geocode=True)[0])
+    print (sbas.open_grids(pairs, 'unwrap', func=[sbas.vertical_displacement_mm, sbas.nearest_grid], mask=sbas.intf_ra2ll(landmask_ra), geocode=True)[0])
+    print (sbas.open_grids(pairs, 'unwrap', func=[sbas.eastwest_displacement_mm, sbas.nearest_grid], mask=sbas.intf_ra2ll(landmask_ra), geocode=True)[0])
