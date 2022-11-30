a29c0169b0db6ea5610fb2c8f3f45e333796478e
Alexey Pechnikov <pechnikov@mobigroup.ru>
2022-11-30 14:01:40 +0700

    Simplify and enhance CI test wrapper script

--- tests/S1A_Stack_CPGF_T173.sh
+++ tests/S1A_Stack_CPGF_T173.sh
@@ -1,31 +1,19 @@
 #!/bin/sh
 
-if [ "$#" -eq 0 ]
-then
-    echo "Default launch saves downloaded dataset for future usage"
-    force=0
-elif [ "$#" -eq 1 ]
-then
-    echo "Force launch removes downloaded dataset to save disk space"
-    force=1
-else
-    echo "Incorrect number of arguments"
-    exit 1
-fi
-
 #wget -q --show-progress --progress=bar:force:noscroll -c http://topex.ucsd.edu/gmtsar/tar/S1A_Stack_CPGF_T173.tar.gz
 ./icloud_download.sh "https://www.icloud.com/iclouddrive/0b2SKaQ9FokMxD75yu6FF5lyg#S1A_Stack_CPGF_T173.tar.gz"
-rm -rf raw_orig topo
-tar xvzf S1A_Stack_CPGF_T173.tar.gz -C .
+# unpack the dataset
+rm -rf raw_orig topo && tar xvzf S1A_Stack_CPGF_T173.tar.gz -C . && rm -f raw_orig/._*
 # cleanup
-if [ "$force" -eq 1 ]
+if [ "$1" != "" ]
 then
-    echo "Removing downloaded datasets to free disk space following 'force' command line argument'..."
+    echo "Removing downloaded datasets to free disk space following 'force' command line argument..."
     rm *.tar.gz
 fi
-echo "Checking disk space and downloaded files"
+
+echo "Checking disk space"
 df -h
-ls -lh
+
 echo "Running Python test script..."
 rm -fr *.jpg
 python3 ./S1A_Stack_CPGF_T173.py && echo SUCCESS
