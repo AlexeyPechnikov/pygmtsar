d6033ef1424989fd0939c1160c1837f63d4bf415
Alexey Pechnikov <pechnikov@mobigroup.ru>
2023-09-04 16:13:37 +0700

    Add initial 1D unwrapping code

--- pygmtsar/pygmtsar/SBAS_unwrap.py
+++ pygmtsar/pygmtsar/SBAS_unwrap.py
@@ -11,6 +11,164 @@ from .SBAS_unwrap_snaphu import SBAS_unwrap_snaphu
 
 class SBAS_unwrap(SBAS_unwrap_snaphu):
 
+    @staticmethod
+    def unwrap1d(data_pairs, matrix):
+        import numpy as np
+
+        # the data variable will be modified and returned as the function output
+        data = data_pairs.copy()
+        #data = phase_pairs.values
+        pair_sum = matrix.sum(axis=1)
+        # tidal displacements for SBAS pairs
+        #tidal_pairs = (matrix * np.concatenate([[0], np.diff(tidal)])).sum(axis=1)
+        # wrapped phase pairs (phase_tidal)
+        #data = (np.mod((phase_pairs - tidal_pairs) + np.pi, 2 * np.pi) - np.pi)
+
+        # pairs do not require unwrapping
+        pairs_ok = []
+
+        # check all compound pairs vs single pairs: only detect all not wrapped
+        for ndate in np.unique(pair_sum)[1:]:
+            for pair_idx in np.where(pair_sum==ndate)[0]:
+                #print (pair_idx, matrix[pair_idx])
+                matching_columns = matrix[pair_idx] == 1
+                #print ('matching_columns', matching_columns)
+                # works for dates = 2
+                #matching_rows = ((matrix[:, matching_columns] == 1).sum(axis=1) == 1)&((matrix[:, ~matching_columns] == 1).sum(axis=1) == 0)
+                matching_rows = ((matrix[:, matching_columns] >= 1).sum(axis=1) == 1)&((matrix[:, ~matching_columns] == 1).sum(axis=1) == 0)
+                #print ('matching_rows', matching_rows)
+                matching_matrix = matrix[:,matching_columns] * matching_rows[:,None]
+                #row_indices = np.where(matching_rows)[0]
+                #print ('row_indices', row_indices)
+                #with np.printoptions(threshold=np.inf, linewidth=np.inf):
+                #    print (matching_matrix.T)
+                value = data[pair_idx]
+                values = (matching_matrix * data[:,None])
+                #print ('value', value, '?=', values.sum())    
+                jump = int(((values.sum() - value) / (2*np.pi)).round())
+                if jump == 0:
+                    pairs_ok.append(pair_idx)
+                    pairs_ok.extend(np.where(matching_rows)[0])
+                    #print ()
+                    continue
+
+                #print ('jump', jump)
+                if abs(value) > abs(value + 2*np.pi*jump):
+                    #print (f'JUMP {ndate}:', jump)
+                    #jump, phase.values[pair_idx] + 2*np.pi*jump
+                    #print ('value', value, '=>', value + 2*np.pi*jump, '=', values.sum())
+                    pass
+                else:
+                    #print (f'JUMP singles:', jump)
+                    pass
+                #print (values[matching_rows])
+                # check for wrapping
+                valid_values = values[matching_rows].ravel()
+                #maxdiff = abs(np.diff(valid_values[valid_values!=0])).max()
+                #print ('maxdiff', maxdiff, maxdiff >= np.pi)
+                #print ()
+                #break
+        #print ('pairs_ok', pairs_ok)
+        #print ('==================')
+
+        # check all compound pairs vs single pairs: fix wrapped compound using not wrapped singles only
+        for ndate in np.unique(pair_sum)[1:]:
+            for pair_idx in np.where(pair_sum==ndate)[0]:
+                if pair_idx in pairs_ok:
+                    continue
+                #print (pair_idx, matrix[pair_idx])
+                matching_columns = matrix[pair_idx] == 1
+                #print ('matching_columns', matching_columns)
+                # works for dates = 2
+                #matching_rows = ((matrix[:, matching_columns] == 1).sum(axis=1) == 1)&((matrix[:, ~matching_columns] == 1).sum(axis=1) == 0)
+                matching_rows = ((matrix[:, matching_columns] >= 1).sum(axis=1) == 1)&((matrix[:, ~matching_columns] == 1).sum(axis=1) == 0)
+                #print ('matching_rows', matching_rows)
+                matching_matrix = matrix[:,matching_columns] * matching_rows[:,None]
+                #row_indices = np.where(matching_rows)[0]
+                #print ('row_indices', row_indices)
+                #with np.printoptions(threshold=np.inf, linewidth=np.inf):
+                #    print (matching_matrix.T)
+                #print ('matching_matrix', matching_matrix.shape)
+
+                pairs_single_not_valid = [pair for pair in np.where(matching_rows)[0] if not pair in pairs_ok]
+                if len(pairs_single_not_valid) > 0:
+                    # some of single-pairs requires unwrapping, miss the compound segment processing
+                    #print ('ERROR')
+                    #print ()
+                    continue
+
+                value = data[pair_idx]
+                values = (matching_matrix * data[:,None])
+                #print ('value', value, '?=', values.sum())    
+                jump = int(((values.sum() - value) / (2*np.pi)).round())
+                #print (f'JUMP {ndate}:', jump)
+                data[pair_idx] += 2*np.pi*jump
+                pairs_ok.append(pair_idx)
+                #print ()
+                continue
+        #print ('==================')
+
+        # check all compound pairs vs single pairs
+        for ndate in np.unique(pair_sum)[1:]:
+            for pair_idx in np.where(pair_sum==ndate)[0]:
+                if pair_idx in pairs_ok:
+                    continue
+                #print (pair_idx, matrix[pair_idx])
+                matching_columns = matrix[pair_idx] == 1
+                #print ('matching_columns', matching_columns)
+                # works for dates = 2
+                #matching_rows = ((matrix[:, matching_columns] == 1).sum(axis=1) == 1)&((matrix[:, ~matching_columns] == 1).sum(axis=1) == 0)
+                matching_rows = ((matrix[:, matching_columns] >= 1).sum(axis=1) == 1)&((matrix[:, ~matching_columns] == 1).sum(axis=1) == 0)
+                #print ('matching_rows', matching_rows)
+                matching_matrix = matrix[:,matching_columns] * matching_rows[:,None]
+                #row_indices = np.where(matching_rows)[0]
+                #print ('row_indices', row_indices)
+                #with np.printoptions(threshold=np.inf, linewidth=np.inf):
+                #    print (matching_matrix.T)
+                value = data[pair_idx]
+                values = (matching_matrix * data[:,None])
+                #print ('value', value, '???=', values.sum())    
+                jump = int(((values.sum() - value) / (2*np.pi)).round())
+                #print ('jump', jump)
+                if jump != 0:
+                    #print (f'JUMP {ndate}:', jump)
+                    #jump, phase.values[pair_idx] + 2*np.pi*jump
+                    #print ('value', value, '=>', value + 2*np.pi*jump, '=', values.sum())
+                    pass
+                #print (values[matching_rows])
+                # unwrap
+                data[pair_idx] += 2*np.pi*jump
+                pairs_ok.append(pair_idx)
+                pairs_ok.extend(np.where(matching_rows)[0])
+                #print ()
+                #break
+    
+        # validity mask
+        mask = [idx in pairs_ok for idx in range(data_pairs.size)]
+        return np.where(mask, data, np.nan)
+
+    def stack_unwrap1d(self, data):
+        import xarray as xr
+        import numpy as np
+        
+        baseline_pairs = self.get_pairs(data)
+        matrix = self.lstsq_matrix(baseline_pairs)
+
+        # xarray wrapper
+        model = xr.apply_ufunc(
+            self.unwrap1d,
+            data.chunk(dict(pair=-1)),
+            dask='parallelized',
+            vectorize=True,
+            input_core_dims=[['pair']],
+            output_core_dims=[['pair']],
+            output_dtypes=[np.float32],
+            kwargs={'matrix': matrix}
+        ).transpose('pair',...)
+
+        return model.rename('unwrap')
+
+    # TODO
     def stack_snaphu(self):
         return
 # 
