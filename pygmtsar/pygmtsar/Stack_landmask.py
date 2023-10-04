15242003861eb03e465a412b04d74fd3cb623804
Alexey Pechnikov <pechnikov@mobigroup.ru>
2023-10-04 15:38:36 +0700

    Update land mask processing functions

--- pygmtsar/pygmtsar/Stack_landmask.py
+++ pygmtsar/pygmtsar/Stack_landmask.py
@@ -36,15 +36,13 @@ class Stack_landmask(Stack_phasediff):
             self.landmask_filename = None
         return self
 
-    def get_landmask(self, inverse_geocode=False):
+    def get_landmask(self):
         """
-        Get the landmask grid in geographic or radar coordinates.
+        Get the landmask grid in geographic coordinates.
 
         Parameters
         ----------
-        inverse_geocode : bool, optional
-            Whether to perform inverse geocoding on the landmask grid. If True, the grid will be transformed from geographic
-            coordinates to radar coordinates. Default is False.
+        None
 
         Returns
         -------
@@ -54,15 +52,11 @@ class Stack_landmask(Stack_phasediff):
         Examples
         --------
         Get land mask in geographic coordinates:
-        landmask_ll = stack.get_landmask()
-
-        Get land mask in radar coordinates:
-        landmask_ra = stack.get_landmask(inverse_geocode=True)
+        landmask = stack.get_landmask()
 
         Notes
         -----
-        This method opens the landmask NetCDF file and extracts the landmask grid. The grid can be cropped to the valid area
-        only or transformed from geographic coordinates to radar coordinates using inverse geocoding.
+        This method opens the landmask NetCDF file and extracts the landmask grid.
         """
         import xarray as xr
         import os
@@ -82,25 +76,16 @@ class Stack_landmask(Stack_phasediff):
         # round the coordinates up to 1 mm to have the same grid for dem and landmask
         landmask['lat'] = landmask.lat.round(8)
         landmask['lon'] = landmask.lon.round(8)
+        return landmask
 
-        # double conversion to unify landmask to interferogram grid in radar coordinates
-        # inverse geocode to radar coordinates grid
-        landmask_ra = self.intf_ll2ra(landmask)
-        if inverse_geocode:
-            return landmask_ra
-
-        # geocode to the exact geographical coordinates grid as interferogram
-        landmask_ll = self.intf_ra2ll(landmask_ra)
-        return landmask_ll
-
-    def download_landmask(self, backend=None, debug=False):
+    def download_landmask(self, product='1s', debug=False):
         """
         Download the landmask and save as NetCDF file.
 
         Parameters
         ----------
-        backend : deprecated
-            This parameter is deprecated and should be omitted.
+        product : str, optional
+                Available options are '1s' (1 arcsec ~= 30m, default) and '3s' (3 arcsec ~= 90m).
         debug : bool, optional
             Whether to enable debug mode. Default is False.
 
@@ -115,36 +100,26 @@ class Stack_landmask(Stack_phasediff):
         import pygmt
         import os
         from tqdm.auto import tqdm
-        # correspond to SRTM3 DEM
-        arcsec_degree = 0.000833333333333/3
 
         if self.landmask_filename is not None:
             print ('NOTE: landmask exists, ignore the command. Use Stack.set_landmask(None) to allow new landmask downloading')
             return
 
-        if backend is not None:
-            print ('Note: backend argument is deprecated, just omit it')
-
         # generate the same as DEM grid
         landmask_filename = os.path.join(self.basedir, 'landmask.nc')
 
         # geographical grid for interferogram area only
-        dem = self.get_intf_ra2ll()[['lat','lon']]
-        scale = dem.lon.diff('lon')[0].item()
+        dem = self.get_dem(geoloc=True)
         llmin = dem.lon.min().item()
         llmax = dem.lon.max().item()
         ltmin = dem.lat.min().item()
         ltmax = dem.lat.max().item()
         region = f'{llmin}/{llmax}/{ltmin}/{ltmax}'
-        #print('region', region)
-
-        # define approximate resolution in arc seconds
-        spacing = (dem.lat.diff('lat')[0]/arcsec_degree).round(3).item()
-        # format as string
-        spacing = f'{spacing}s'
+        if debug:
+            print('region', region)
 
         with tqdm(desc='Landmask Downloading', total=1) as pbar:
-            landmask = pygmt.grdlandmask(resolution='f', region=region, spacing=spacing, maskvalues='NaN/1')
+            landmask = pygmt.grdlandmask(resolution='f', region=region, spacing=product, maskvalues='NaN/1')
             if os.path.exists(landmask_filename):
                 os.remove(landmask_filename)
             landmask.to_netcdf(landmask_filename)
