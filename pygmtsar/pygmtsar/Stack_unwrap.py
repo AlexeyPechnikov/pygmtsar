433905480bb22bb9c621a3239f65f312ba9a0a53
Alexey Pechnikov <pechnikov@mobigroup.ru>
2023-10-05 22:08:46 +0700

    Add delayed unwrap2d_snaphu() function

--- pygmtsar/pygmtsar/Stack_unwrap.py
+++ pygmtsar/pygmtsar/Stack_unwrap.py
@@ -179,91 +179,48 @@ class Stack_unwrap(Stack_unwrap_snaphu):
 
         return model.rename('unwrap')
 
-    # TODO
-    def stack_snaphu(self):
-        return
-# 
-#     def snaphu_parallel(self, pairs=None, mask=None, chunksize=None, n_jobs=-1, **kwargs):
-#         """
-#         Unwraps phase using SNAPHU in parallel for multiple interferogram pairs.
-# 
-#         This function unwraps the phase of multiple interferograms using the Statistical-cost, Network-flow Algorithm
-#         for Phase Unwrapping (SNAPHU) with user-defined parameters. The unwrapped phase is saved as grid files
-#         in the working directory. This function is designed to process multiple interferograms in parallel, 
-#         leveraging the available computational resources.
-# 
-#         Parameters
-#         ----------
-#         pairs : list, tuple, array or pandas.DataFrame, optional
-#             A list or array of pairs of reference and repeat dates, or a DataFrame with 'ref' and 'rep' columns.
-# 
-#         mask : str or xarray.DataArray, optional
-#             A user-defined mask for radar coordinates, default is None.
-# 
-#         n_jobs : int, optional
-#             The number of jobs to run in parallel. -1 means using all available processors, default is -1.
-# 
-#         **kwargs : dict
-#             Additional keyword arguments to be passed to the unwrap function.
-# 
-#         Returns
-#         -------
-#         None
-#             This function saves the unwrapped phase to disk as grid files and does not return any values.
-# 
-#         Examples
-#         --------
-#         Simplest unwrapping:
-#         stack.unwrap_parallel(pairs)
-# 
-#         Filter low-coherence areas with common correlation threshold 0.75:
-#         stack.unwrap_parallel(pairs, threshold=0.075)
-# 
-#         Unwrap with coherence threshold 0.075 and fill NODATA gaps:
-#         interpolator = lambda corr, unwrap: stack.nearest_grid(unwrap).where(corr>=0)
-#         stack.unwrap_parallel(pairs, threshold=0.075, func=interpolator)
-# 
-#         Unwrap with coherence threshold 0.075 and apply land mask:
-#         cleaner = lambda corr, unwrap: xr.where(corr>=0.075, unwrap, np.nan)
-#         stack.unwrap_parallel(pairs, threshold=0.075, mask=landmask_ra, func=cleaner)
-# 
-#         Unwrap with coherence threshold 0.075 and use SNAPHU tiling for faster processing and smaller RAM usage:
-#         cleaner = lambda corr, unwrap: xr.where(corr>=0.075, unwrap, np.nan)
-#         conf = stack.PRM().snaphu_config(NTILEROW=1, NTILECOL=2, ROWOVRLP=200, COLOVRLP=200)
-#         stack.unwrap_parallel(pairs, n_jobs=1, threshold=0.075, func=cleaner, conf=conf)
-# 
-#         Notes
-#         -----
-#         This function unwraps phase using SNAPHU in parallel for multiple interferogram pairs.
-#         The unwrapped phase is saved as grid files in the working directory.
-#         Additional keyword arguments can be passed to customize the unwrapping process.
-#         """
-#         import xarray as xr
-#         import pandas as pd
-#         from tqdm.auto import tqdm
-#         import joblib
-#         import os
-# 
-#         # for now (Python 3.10.10 on MacOS) joblib loads the code from disk instead of copying it
-#         kwargs['chunksize'] = chunksize
-# 
-#         # convert pairs (list, array, dataframe) to 2D numpy array
-#         pairs = self.get_pairs(pairs)[['ref', 'rep']].astype(str).values
-#         
-#         # materialize lazy mask
-# #         if mask is not None and isinstance(mask, xr.DataArray):
-# #             mask_filename = self.get_filenames(None, 'unwrapmask')
-# #             if os.path.exists(mask_filename):
-# #                 os.remove(mask_filename)
-# #             # workaround to save NetCDF file correct
-# #             #if 'y' in mask.dims and 'x' in mask.dims:
-# #             #    mask = mask.rename({'y':'a', 'x':'r'})
-# #             mask.rename('mask').to_netcdf(mask_filename, encoding={'mask': self.compression(mask.shape, chunksize=chunksize)}, engine=self.netcdf_engine)
-# #             kwargs['mask'] = 'unwrapmask'
-# 
-#         # save results to NetCDF files
-#         kwargs['interactive'] = False
-# 
-#         with self.tqdm_joblib(tqdm(desc='SNAPHU Unwrapping', total=len(pairs))) as progress_bar:
-#             joblib.Parallel(n_jobs=n_jobs)(joblib.delayed(self.snaphu)(pair, tiledir=f'snaphu_tiledir_{pair[0]}_{pair[1]}', **kwargs) for pair in pairs)
-# # 
+    def unwrap2d_snaphu(self, phase, corr=None, conf=None, conncomp=False):
+        """
+        Use this wrapper for tiled multicore SNAPHU configuration:
+        with dask.config.set(scheduler='single-threaded'):
+            stack.unwrap2d_snaphu()...).phase.compute()
+        
+        """
+        import xarray as xr
+        import numpy as np
+        import dask
+
+        # output dataset variables
+        keys = {0: 'phase', 1: 'conncomp'}
+
+        if len(phase.dims) == 2:
+            stackvar = None
+        else:
+            stackvar = phase.dims[0]
+
+        if corr is not None:
+            assert phase.shape == corr.shape, 'ERROR: phase and correlation variables have different shape'
+
+        def snaphu(ind):
+            ds = self.snaphu(phase[ind] if stackvar is not None else phase,
+                             corr[ind]  if stackvar is not None and corr is not None else corr,
+                             conf=conf, conncomp=conncomp)
+            return ds.to_array()
+
+        stack =[]
+        for ind in range(len(phase) if stackvar is not None else 1):
+            block = dask.array.from_delayed(dask.delayed(snaphu)(ind),
+                        shape=(2 if conncomp else 1, *(phase.shape[1:] if stackvar is not None else phase.shape)),
+                        dtype=np.float32)
+            stack.append(block)
+            del block
+        dask_block = dask.array.concatenate(stack)
+        del stack
+        if stackvar is not None:
+            ds = xr.merge([xr.DataArray(dask_block[idx::2 if conncomp else 1], coords=phase.coords).rename(keys[idx])
+                               for idx in range(2 if conncomp else 1)])
+        else:
+            ds = xr.merge([xr.DataArray(block, coords=phase.coords).rename(keys[idx])
+                           for idx, block in enumerate(dask_block)])
+        del dask_block
+        return ds
