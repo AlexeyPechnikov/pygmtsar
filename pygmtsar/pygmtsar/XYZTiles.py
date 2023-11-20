e3deb8deb5d22c83e050343ec17b107894b92a6d
Alexey Pechnikov <pechnikov@mobigroup.ru>
2023-11-20 15:27:57 +0700

    Fix 1 pixel overlap between tiles, allow Xarray objects as the geometry. Prepare the output to process in the VTK wrapper.

--- pygmtsar/pygmtsar/XYZTiles.py
+++ pygmtsar/pygmtsar/XYZTiles.py
@@ -49,25 +49,41 @@ class XYZTiles(tqdm_joblib):
         # latitudes inverted
         #lat_start, lat_end = 34.42, 34.36
         #lon_start, lon_end = -118.5, -118.4
-        lon_start, lat_start, lon_end, lat_end = geometry.bounds
+        if isinstance(geometry, (xr.DataArray, xr.Dataset)):
+            lon_start = geometry.lon.min()
+            lat_start = geometry.lat.min()
+            lon_end   = geometry.lon.max()
+            lat_end   = geometry.lat.max()
+        else:
+            lon_start, lat_start, lon_end, lat_end = geometry.bounds
         lat_start, lat_end = lat_end, lat_start
 
         # Calculate tile range
         x_start, y_start = deg2num(lat_start, lon_start, zoom)
         x_end, y_end = deg2num(lat_end, lon_end, zoom)
 
+    #     def job_tile(x, y):
+    #         tile_image = download_tile(url, x, y, zoom)
+    #         tile_array = np.array(tile_image)
+    #         lat_upper_left, lon_upper_left = num2deg(x, y, zoom)
+    #         lat_lower_right, lon_lower_right = num2deg(x + 1, y + 1, zoom)
+    #         latitudes = np.linspace(lat_upper_left, lat_lower_right, tile_array.shape[0])
+    #         longitudes = np.linspace(lon_upper_left, lon_lower_right, tile_array.shape[1])
+    #         return xr.DataArray(tile_array, dims=('lat', 'lon', 'band'), 
+    #                                coords={'lat': latitudes, 'lon': longitudes}).rename('tile')
         def job_tile(x, y):
             tile_image = download_tile(url, x, y, zoom)
             tile_array = np.array(tile_image)
             lat_upper_left, lon_upper_left = num2deg(x, y, zoom)
             lat_lower_right, lon_lower_right = num2deg(x + 1, y + 1, zoom)
-            latitudes = np.linspace(lat_upper_left, lat_lower_right, tile_array.shape[0])
-            longitudes = np.linspace(lon_upper_left, lon_lower_right, tile_array.shape[1])
+            # Exclude the endpoint to prevent overlap with the adjacent tile
+            latitudes = np.linspace(lat_upper_left, lat_lower_right, tile_array.shape[0], endpoint=False)
+            longitudes = np.linspace(lon_upper_left, lon_lower_right, tile_array.shape[1], endpoint=False)
             return xr.DataArray(tile_array, dims=('lat', 'lon', 'band'), 
-                                   coords={'lat': latitudes, 'lon': longitudes}).rename('tile')
-        
+                                coords={'lat': latitudes, 'lon': longitudes}).rename('colors')
+
         with self.tqdm_joblib(tqdm(desc='XYZ Tile Downloading', total=(x_end-x_start+1)*(y_end-y_start+1))) as progress_bar:
             tile_xarrays = joblib.Parallel(n_jobs=n_jobs)(joblib.delayed(job_tile)(x, y)\
                                 for x in range(x_start, x_end + 1) for y in range(y_start, y_end + 1))
 
-        return xr.combine_by_coords(tile_xarrays)['tile']
+        return xr.combine_by_coords(tile_xarrays)['colors'].transpose('band', 'lat', 'lon')
