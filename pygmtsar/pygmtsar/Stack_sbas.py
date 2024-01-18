a1e67ef71484dc6e541fa0fd5309c7b503ab2c2f
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-01-18 18:34:51 +0700

    Add 'aspect' parameter to plot_displacement() function

--- pygmtsar/pygmtsar/Stack_sbas.py
+++ pygmtsar/pygmtsar/Stack_sbas.py
@@ -905,7 +905,7 @@ class Stack_sbas(Stack_detrend):
         return np.sqrt((weight * error).sum('pair') / weight.sum('pair') / len(pairs))
 
     @staticmethod
-    def plot_displacement(data, caption='Cumulative LOS Displacement, [rad]', quantile=None, vmin=None, vmax=None):
+    def plot_displacement(data, caption='Cumulative LOS Displacement, [rad]', quantile=None, vmin=None, vmax=None, aspect=None):
         import numpy as np
         import matplotlib.pyplot as plt
 
@@ -918,6 +918,8 @@ class Stack_sbas(Stack_detrend):
         plt.figure(figsize=(12,4), dpi=300)
         data.plot.imshow(vmin=vmin, vmax=vmax, cmap='turbo')
         plt.title(caption, fontsize=18)
+        if aspect is not None:
+            plt.gca().set_aspect(aspect)
         plt.show()
 
     @staticmethod
