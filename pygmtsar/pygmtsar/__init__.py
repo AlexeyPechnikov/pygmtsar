a4eef087210e44e83cfac9bdb851634d4b8a994d
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-02-25 19:06:22 +0700

    Add version postfix

--- pygmtsar/pygmtsar/__init__.py
+++ pygmtsar/pygmtsar/__init__.py
@@ -7,7 +7,7 @@
 # 
 # Licensed under the BSD 3-Clause License (see LICENSE for details)
 # ----------------------------------------------------------------------------
-__version__ = '2024.2.21'
+__version__ = '2024.2.21.post1'
 
 # unified progress indicators
 from .tqdm_joblib import tqdm_joblib
