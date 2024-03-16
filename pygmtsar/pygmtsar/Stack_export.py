f20ffbcaba840703c8039f3a4bfe086ca006c5c1
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-03-17 01:57:50 +0700

    Prepare to remove module NCubeVTK

--- pygmtsar/pygmtsar/Stack_export.py
+++ pygmtsar/pygmtsar/Stack_export.py
@@ -9,7 +9,6 @@
 # ----------------------------------------------------------------------------
 from .Stack_ps import Stack_ps
 from .tqdm_dask import tqdm_dask
-from .NCubeVTK import NCubeVTK
 
 class Stack_export(Stack_ps):
 
@@ -54,6 +53,109 @@ class Stack_export(Stack_ps):
             y_dim = 'y'
             x_dim = 'x'
         return da.rio.write_crs(epsg).rio.set_spatial_dims(y_dim=y_dim, x_dim=x_dim)
+
+    @staticmethod
+    def as_vtk(dataset):
+        """
+        Map a 2D image onto a 3D topography.
+
+        Parameters:
+            dataset: A file name or Xarray Dataset to use for mapping the image.
+
+        Returns:
+            A vtkStructuredGrid representing the image mapped onto the topography.
+        """
+        from vtk import vtkPoints, vtkStructuredGrid, vtkThreshold, vtkDataObject, \
+            VTK_FLOAT, VTK_UNSIGNED_CHAR, vtkStringArray, vtkFloatArray, vtkIntArray
+        from vtk.util import numpy_support as vn
+        import numpy as np
+        import xarray as xr
+
+        assert isinstance(dataset, xr.Dataset), 'ERROR: Expected "dataset" argument as Xarray Dataset'
+
+        # fill NODATA by NAN
+        for data_var in dataset.data_vars:
+            #if dataset[data_var].values.dtype in [np.dtype('float16'),np.dtype('float32'),np.dtype('float64')]:
+            if np.issubdtype(dataset[data_var].dtype, np.floating):
+                dataset[data_var].values = dataset[data_var].values.astype('float32')
+                if '_FillValue' in dataset[data_var] and not np.isnan(dataset[data_var]._FillValue):
+                    dataset[data_var].values[dataset[data_var].values == dataset[data_var]._FillValue] = np.nan
+
+        xs = dataset.x.values
+        ys = dataset.y.values
+        (yy,xx) = np.meshgrid(ys, xs)
+        if 'z' in dataset:
+            zs = dataset.z.values
+        else:
+            # set z coordinate to zero
+            zs = np.zeros_like(xx)
+
+        # create raster mask by geometry and for NaNs
+        vtk_points = vtkPoints()
+        points = np.column_stack((xx.ravel('F'), yy.ravel('F'), zs.ravel('C')))
+        vtk_points.SetData(vn.numpy_to_vtk(points, deep=True))
+
+        sgrid = vtkStructuredGrid()
+        sgrid.SetDimensions(len(xs), len(ys), 1)
+        sgrid.SetPoints(vtk_points)
+
+        for data_var in dataset.data_vars:
+            if 'z' == data_var:
+                # variable is already included into coordinates
+                continue
+            da = dataset[data_var]
+            dims = da.dims
+            #print (data_var, dims, da.dtype)
+            if 'band'in dims:
+                bands = da.band.shape[0]
+                #print ('bands', bands)
+                if bands in [3,4]:
+                    # RGB or RGBA, select 3 bands only
+                    array = vn.numpy_to_vtk(da[:3].values.reshape(3,-1).T, deep=True, array_type=VTK_UNSIGNED_CHAR)
+                    array.SetName(da.name)
+                    sgrid.GetPointData().AddArray(array)
+                elif bands == 1:
+                    array = vn.numpy_to_vtk(da.values.reshape(1,-1).T, deep=True, array_type=VTK_FLOAT)
+                    array.SetName(da.name)
+                    sgrid.GetPointData().AddArray(array)
+                else:
+                    print (f'ERROR: Unsupported bands count (should be 1,3 or 4) {bands} for variable {data_var}')
+                    return
+            else:
+                array = vn.numpy_to_vtk(da.values.ravel(), deep=True, array_type=VTK_FLOAT)
+                array.SetName(da.name)
+                sgrid.GetPointData().AddArray(array)
+
+        for coord in dataset.coords:
+            #print (coord, dataset[coord].dims, len(dataset[coord].dims))
+            if len(dataset[coord].dims) > 0:
+                # real coordinate, ignore it
+                continue
+            #print (coord, dataset[coord].dtype)
+            #print ('np.datetime64', np.issubdtype(dataset[coord].dtype, np.datetime64))
+            #print ('np.int64', np.issubdtype(dataset[coord].dtype, np.int64))
+            #print ('np.float64', np.issubdtype(dataset[coord].dtype, np.float64))
+            if np.issubdtype(dataset[coord].dtype, np.datetime64):
+                data_array = vtkStringArray()
+                data_value = str(dataset[coord].dt.date.values)
+            elif np.issubdtype(dataset[coord].dtype, str):
+                data_array = vtkStringArray()
+                data_value = str(dataset[coord].values)
+            elif np.issubdtype(dataset[coord].dtype, np.int64):
+                data_array = vtkIntArray()
+                data_value = dataset[coord].values.astype(np.int64)
+            elif np.issubdtype(dataset[coord].dtype, np.float64):
+                data_array = vtkFloatArray
+                data_value = dataset[coord].values.astype(np.float64)
+            else:
+                print(f'NOTE: unsupported attribute {coord} datatype {dataset[coord].dtype}, miss it')
+                continue
+            data_array.SetName(coord)
+            data_array.InsertNextValue(data_value)
+            sgrid.GetFieldData().AddArray(data_array)
+
+        # required for 3D interactive rendering on Google Colab
+        return sgrid
 
     def export_geotiff(self, data, name, caption='Exporting WGS84 GeoTIFF(s)'):
         """
@@ -245,7 +347,7 @@ class Stack_export(Stack_ps):
         tqdm_dask(dask.persist(delayed), desc=caption)
         del grid
 
-    def export_vtk(self, data, name, caption='Exporting WGS84 VTK(s)', topo='auto', image=None, band_mask=None, use_sealevel=False):
+    def export_vtk(self, data, name, caption='Exporting WGS84 VTK(s)', topo='auto', image=None, mask=None):
         """
         Export VTK file "velocity.vtk":
         sbas.export_vtk(velocity, 'velocity')
@@ -289,7 +391,11 @@ class Stack_export(Stack_ps):
             # convert topography to geographic coordinates if necessary
             dem = self.ra2ll(topo) if not self.is_geo(topo) else topo
         if topo is not None:
-            dem = dem.reindex_like(grid2d, method='nearest').where(np.isfinite(grid2d))
+            dem = dem.reindex_like(grid2d, method='nearest')
+            if isinstance(mask, str) and mask == 'auto':
+                dem = dem.where(np.isfinite(grid2d))
+            elif mask is not None:
+                dem = dem.where(mask.reindex_like(grid2d, method='nearest'))
             dss.append(dem.rename('z'))
     
         if image is not None:
@@ -310,7 +416,7 @@ class Stack_export(Stack_ps):
                     filename = f'{name}.vtk'
                 #print ('ds', ds)
                 
-                vtk_grid = NCubeVTK.ImageOnTopography(ds)
+                vtk_grid = self.as_vtk(ds)
                 if stackval is not None:
                     metadata = vtkStringArray()
                     metadata.SetName(stackvar)
