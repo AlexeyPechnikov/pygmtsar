468c0b07d69bbf5d5700e6e188fcdb348aaaca70
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-08-03 23:01:46 +0700

    Decrease bursts transparency in plot_scenes()

--- pygmtsar/pygmtsar/Stack.py
+++ pygmtsar/pygmtsar/Stack.py
@@ -136,10 +136,8 @@ class Stack(Stack_export):
 
         # Calculate overlaps including self-overlap
         overlap_count = [sum(1 for geom2 in gdf.geometry if geom1.intersects(geom2)) for geom1 in gdf.geometry]
-        # define transparency for the calculated overlaps
-        gdf_alpha = 1/max(overlap_count)
-        # apply minimum transparency threshold
-        gdf.reset_index().plot(color=[colors[k] for k in gdf.index], alpha=max(gdf_alpha, 0.002), edgecolor='black', ax=plt.gca())
+        # define transparency for the calculated overlaps and apply minimum transparency threshold
+        gdf.reset_index().plot(color=[colors[k] for k in gdf.index], alpha=max(2/max(overlap_count), 0.002), edgecolor='black', ax=plt.gca())
         self.plot_AOI(**kwargs)
         self.plot_POI(**kwargs)
         if aspect is not None:
