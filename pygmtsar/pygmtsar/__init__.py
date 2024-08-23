d6b74ad8fc1f898d85955ac10ddc7365c2d25ab2
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-08-23 21:03:28 +0700

    Increase library version number

--- pygmtsar/pygmtsar/__init__.py
+++ pygmtsar/pygmtsar/__init__.py
@@ -7,7 +7,7 @@
 # 
 # Licensed under the BSD 3-Clause License (see LICENSE for details)
 # ----------------------------------------------------------------------------
-__version__ = '2024.8.2.post3'
+__version__ = '2024.8.23'
 
 # unified progress indicators
 from .tqdm_joblib import tqdm_joblib
